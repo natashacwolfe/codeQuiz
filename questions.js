544b6968cc3b3d7e5ed741c75930327b2f34db19
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-01-01 20:03:51 -0800

    added timer function

---
+++ questions.js
@@ -0,0 +1,29 @@
+let questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "2 The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+        title: "3 Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+      },
+      {
+        title: "4 The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+      },
+      {
+        title: "5 The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+      }
+  
+  ];
+  
--- script.js
+++ script.js
@@ -3,38 +3,7 @@ let contentEl = document.querySelector(".content");
 let h1El = document.querySelector("h1");
 
 
-let questions = [
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "2 The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    {
-        title: "3 Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
-      },
-      {
-        title: "4 The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
-      },
-      {
-        title: "5 The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
-      }
-  
-  ];
-  
-
-let timer = 75;
-
+let timer = 0;
 
 
 function codeQuiz(){
@@ -43,28 +12,34 @@ function codeQuiz(){
    
 
     h1El.textContent = "Coding Quiz Challange";
-    infoEl.innerHTML = "Try to answer the following code-related questions within the time <br> limit. Keep in mind that an incorrect answer will penalize your score/time <br> br ten seconds!";
+    infoEl.innerHTML = "Try to answer the following code-related questions within the time limit. Keep in mind that an incorrect answer will penalize your score/time 15 seconds!";
     startEl.textContent = "Start Quiz";
-    h1El.appendChild(infoEl);
-    h1El.appendChild(startEl);
+    contentEl.appendChild(infoEl);
+    contentEl.appendChild(startEl);
 
     //Start button quiz begins
 
     startEl.addEventListener("click", function(){
-        let choice1 = document.createElement("button");
-        let choice2 = document.createElement("button");
-        let choice3 = document.createElement("button");
-        let choice4 = document.createElement("button");
+        
+        
+        let timer = 75;
+        let timeInterval = setInterval(function(){
+            countEl.textContent = timer;
+            timer --;
+
+
+            let choice1 = document.createElement("button");
+            let choice2 = document.createElement("button");
+            let choice3 = document.createElement("button");
+            let choice4 = document.createElement("button");
+
+            let correctEl = document.createElement("p");
 
 
 
-        let correctEl = document.createElement("p");
+        }, 1000)
+       
 
-        for (let i = 0; i < questions.length; i++){
-            let currentQuestion = questions[i];
-                
-
-        }
      
 
 
