c3923193536f116bc9e2cb056599a3f93143c92c
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-01-03 12:23:21 -0800

    working on choices

--- script.js
+++ script.js
@@ -1,54 +1,96 @@
-let countEl = document.querySelector("#count");
-let contentEl = document.querySelector(".content");
-let h1El = document.querySelector("h1");
+let count = document.querySelector("#count");
+let contentBody = document.querySelector(".content");
+let quizTitle = document.querySelector("h1");
 
 
 let timer = 0;
 
 
+
+
 function codeQuiz(){
-    let infoEl = document.createElement("h5");
-    let startEl = document.createElement("button");
+    let info = document.createElement("h5");
+    let startButton = document.createElement("button");
    
 
-    h1El.textContent = "Coding Quiz Challange";
-    infoEl.innerHTML = "Try to answer the following code-related questions within the time limit. Keep in mind that an incorrect answer will penalize your score/time 15 seconds!";
-    startEl.textContent = "Start Quiz";
-    contentEl.appendChild(infoEl);
-    contentEl.appendChild(startEl);
+    quizTitle.textContent = "Coding Quiz Challange";
+    info.innerHTML = "Try to answer the following code-related questions within the time limit. Keep in mind that an incorrect answer will penalize your score/time 15 seconds!";
+    startButton.textContent = "Start Quiz";
+    contentBody.appendChild(info);
+    contentBody.appendChild(startButton);
 
     //Start button quiz begins
 
-    startEl.addEventListener("click", function(){
-        
+    startButton.addEventListener("click", function(){
+        quizTitle.textContent = "";
+        info.remove();
+        startButton.remove();
         
         let timer = 75;
         let timeInterval = setInterval(function(){
-            countEl.textContent = timer;
+            count.textContent = timer;
             timer --;
 
+            //if (secondsLeft === 0) {
+            //     clearInterval(timeInterval);
+            //     alert highscore;
+            // }
+            
+            
+    }, 1000)
+            
+    renderQuestion();
+    renderChoices();
+            function renderQuestion(){
+                let q = 0;
+                let currentQuestion = questions[q].title;
+                
 
-            let choice1 = document.createElement("button");
-            let choice2 = document.createElement("button");
-            let choice3 = document.createElement("button");
-            let choice4 = document.createElement("button");
+                quizTitle.innerHTML = currentQuestion;
 
-            let correctEl = document.createElement("p");
+                 
+            }
+                function renderChoices(){  
+                    let buttonDiv = document.createElement("div")
+                    let choiceButtons = document.createElement("button");
 
+                    contentBody.appendChild(buttonDiv);
+                    buttonDiv.appendChild(choiceButtons);
+                
+                    for (let choiceOption = 0; choiceOption < questions[q].choices.length; choiceOption++){
+                        let choiceText = questions[q].choice[choiceOption];
+                        
 
+                      
+                        console.log(choiceText);
+                        choiceButton.innerHTML = choiceText;
 
-        }, 1000)
-       
+                    }
+                }
+          
 
-     
-
-
-      
-    })
-
-
-
+            
+                
+                    
     
+         
+
+
+    })
 }
 
 codeQuiz();
+
+
+
+// start quiz
+
+// start timer 75--
+
+// load question and answer choices 
+
+// if the answer choice is correct - say correct - load next question
+
+// if the answer is incorrect - say incorrect - minus 15 seconds - load next question
+
+// all questions are done or timer === 0 then prompt to add initals to highscore
